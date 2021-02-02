d111223cb7f95f9163d235c56c08d356de31b87f
nickKearns <nickkearns133@gmail.com>
2021-02-02 16:53:17 -0500

    prints html file stdout

--- makesite.go
+++ makesite.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+// Content for body of txt file
+type Content struct {
+	Body string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	content, err := ioutil.ReadFile("first-post.txt")
+	contentAsString := string(content)
+	contentStruct := Content{contentAsString}
+	// if err != nil {
+	// 	// A common use of `panic` is to abort if a function returns an error
+	// 	// value that we don’t know how to (or want to) handle. This example
+	// 	// panics if we get an unexpected error when creating a new file.
+	// 	panic(err)
+	// }
+	// fmt.Print(string(fileContents))
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	err = t.Execute(os.Stdout, contentStruct)
+	if err != nil {
+		panic(err)
+	}
 }
